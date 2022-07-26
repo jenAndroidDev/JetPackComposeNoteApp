a5885da9ad51d7e67f9a06c9250d502f739ee619
jenAndroidDev <104613017+jenAndroidDev@users.noreply.github.com>
2022-07-26 11:29:40 +0530

    Initial commit

---
+++ app/src/main/java/com/jenin/jetpackcomposenoteapp/screen/NoteScreen.kt
@@ -0,0 +1,33 @@
+package com.jenin.jetpackcomposenoteapp.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jenin.jetpackcomposenoteapp.R
+
+@Composable
+fun NoteScreen(){
+    
+   Column(modifier = Modifier.padding(6.dp)) {
+       TopAppBar(
+           title = { Text(text = stringResource(id = R.string.app_name))},
+           actions = { Icon(imageVector = Icons.Rounded.Notifications, contentDescription = "icon")})
+
+   }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NoteScreenPreview(){
+    NoteScreen()
+}
