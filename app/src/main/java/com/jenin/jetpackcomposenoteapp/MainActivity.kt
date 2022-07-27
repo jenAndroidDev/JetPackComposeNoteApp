cd9931033bd5d94fe560d06bbbc16ae38e4f287c
jenAndroidDev <104613017+jenAndroidDev@users.noreply.github.com>
2022-07-27 15:17:46 +0530

    Data Class added and proceeding with hilt,viewmodels for compose

--- app/src/main/java/com/jenin/jetpackcomposenoteapp/MainActivity.kt
+++ app/src/main/java/com/jenin/jetpackcomposenoteapp/MainActivity.kt
@@ -3,13 +3,13 @@ package com.jenin.jetpackcomposenoteapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
+import com.jenin.jetpackcomposenoteapp.data.loadNotes
+import com.jenin.jetpackcomposenoteapp.model.NoteDataClass
 import com.jenin.jetpackcomposenoteapp.screen.NoteScreen
 import com.jenin.jetpackcomposenoteapp.ui.theme.JetPackComposeNoteAppTheme
 
@@ -19,19 +19,29 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetPackComposeNoteAppTheme {
                 // A surface container using the 'background' color from the theme
+                val notes = remember{
+                    mutableListOf<NoteDataClass>()
+                }
                 Surface(color = MaterialTheme.colors.background) {
-                      NoteScreen()
+                      NoteScreen(
+                          context = this@MainActivity,
+                          note = notes,
+                          onAddNote = {addNote->
+                              notes.add(addNote)
+                          },
+                          onRemoveNote = {removeNote->
+                            notes.remove(removeNote)
+                          })
                 }
             }
         }
     }
 
-
     @Preview(showBackground = true)
     @Composable
     fun DefaultPreview() {
         JetPackComposeNoteAppTheme {
-
+            NoteScreen(context = this@MainActivity, note = loadNotes(),{},{})
         }
     }
 }
---
+++ app/src/main/java/com/jenin/jetpackcomposenoteapp/data/NoteDataSource.kt
@@ -0,0 +1,20 @@
+package com.jenin.jetpackcomposenoteapp.data
+
+
+import com.jenin.jetpackcomposenoteapp.model.NoteDataClass
+
+fun loadNotes():List<NoteDataClass>{
+    return listOf(
+        NoteDataClass(title = "A good day", description = "Watching movies", entryDateTime = "29:07:2022"),
+        NoteDataClass(title = "A good day", description = "Watching movies", entryDateTime = "29:07:2022"),
+        NoteDataClass(title = "A good day", description = "Watching movies", entryDateTime = "29:07:2022"),
+        NoteDataClass(title = "A good day", description = "Watching movies", entryDateTime = "29:07:2022"),
+        NoteDataClass(title = "A good day", description = "Watching movies", entryDateTime = "29:07:2022"),
+        NoteDataClass(title = "A good day", description = "Watching movies", entryDateTime = "29:07:2022"),
+        NoteDataClass(title = "A good day", description = "Watching movies", entryDateTime = "29:07:2022"),
+        NoteDataClass(title = "A good day", description = "Watching movies", entryDateTime = "29:07:2022"),
+        NoteDataClass(title = "A good day", description = "Watching movies", entryDateTime = "29:07:2022"),
+        NoteDataClass(title = "A good day", description = "Watching movies", entryDateTime = "29:07:2022"),
+        NoteDataClass(title = "A good day", description = "Watching movies", entryDateTime = "29:07:2022"),
+    )
+}
